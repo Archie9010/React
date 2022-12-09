9f782913df59f31a048fe3e22d6d65fd0f5c2d06
Archie9010 <archie9010@protonmail.com>
2022-12-09 13:37:07 +0000

    add commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps />
+     <StatefulGreetingWithCallback />
     </div>
   );
 }
